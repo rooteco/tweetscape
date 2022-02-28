b13d278dc914b5c84a548270854032609fa55c00
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-02-28 11:47:41 -0800

    refactor: split articles fetch api

--- app/routes/articles.$topic.ts
+++ app/routes/articles.$topic.ts
@@ -2,47 +2,29 @@ import type { ActionFunction } from 'remix';
 import invariant from 'tiny-invariant';
 import { json } from 'remix';
 
-import type { Article, Influencer, Tweet, TweetRef, URL } from '~/types.server';
-import { caps, decode, log } from '~/utils.server';
+import type { Article, Influencer, Tweet } from '~/types.server';
+import { caps, fetchFromCache, log } from '~/utils.server';
 import { topic } from '~/cookies.server';
 
-async function fetchFromCache(
-  url: string,
-  init?: RequestInit
-): Promise<Response> {
-  const cacheKey = new Request(new URL(url).toString(), init);
-  const cache = caches.default;
-  let res = await cache.match(cacheKey);
-  if (!res) {
-    log.trace(`Cache miss for: ${url}`);
-    res = await fetch(cacheKey);
-    res = new Response(res.body, res);
-    res.headers.append('Cache-Control', `s-maxage=${24 * 60 * 60}`);
-    await cache.put(cacheKey, res.clone());
-  } else {
-    log.trace(`Cache hit for: ${url}`);
-  }
-  return res;
-}
-
-function logTweet(t: Tweet, a: Influencer, url: string, tweets: Tweet[]): void {
+function logTweet(t: Tweet, a: Article): void {
   log.debug('============================================================');
+  if (t.author)
+    log.debug(
+      `Author: ${t.author_id} | https://hive.one/p/${
+        t.author.social_account.social_account.screen_name
+      } | ${t.author.social_account.social_account.name} | @${
+        t.author.social_account.social_account.screen_name
+      } | Rank: ${
+        t.author.rank
+      } | Insider Score: ${t.author.insider_score.toFixed(
+        2
+      )} | Attention Score: ${t.author.attention_score.toFixed(
+        2
+      )} | Weekly Change: ${t.author.attention_score_change_week.toFixed(2)}`
+    );
+  log.debug(`Link: ${a.url} | ${a.tweets.length} tweets`);
   log.debug(
-    `Author: ${a.social_account.social_account.id} | https://hive.one/p/${
-      a.social_account.social_account.screen_name
-    } | ${a.social_account.social_account.name} | @${
-      a.social_account.social_account.screen_name
-    } | Rank: ${a.rank} | Insider Score: ${a.insider_score.toFixed(
-      2
-    )} | Attention Score: ${a.attention_score.toFixed(
-      2
-    )} | Weekly Change: ${a.attention_score_change_week.toFixed(2)}`
-  );
-  log.debug(`Link: ${url} | ${tweets.length} tweets`);
-  log.debug(
-    `Tweet: https://twitter.com/` +
-      `${a.social_account.social_account.screen_name}/status/${t.id}` +
-      `\n\n${t.text}\n`
+    `Tweet: https://twitter.com/${t.author_id}/status/${t.id}\n\n${t.text}\n`
   );
 }
 
@@ -64,7 +46,7 @@ async function getInfluencers(t: string, n = 0): Promise<HiveData> {
     { headers: { authorization: `Token ${HIVE_TOKEN}` } }
   );
   const headers = Object.fromEntries(res.headers.entries());
-  log.debug(`Headers: ${JSON.stringify(headers, null, 2)}`);
+  log.trace(`Headers: ${JSON.stringify(headers, null, 2)}`);
   const data = (await res.json()) as HiveData;
   return {
     ...data,
@@ -73,163 +55,27 @@ async function getInfluencers(t: string, n = 0): Promise<HiveData> {
   };
 }
 
-interface TwitterData {
-  data?: Tweet[];
-  includes?: {
-    users?: { id: string; name: string; username: string }[];
-    tweets?: Tweet[];
-  };
-  meta?: {
-    newest_id?: string;
-    oldest_id?: string;
-    result_count?: number;
-    next_token?: string;
-  };
-  errors?: { parameters: Record<string, string[]>; message: string }[];
-  title?: string;
-  detail?: string;
-  type?: string;
-}
-
-async function getTweets(influencer: Influencer): Promise<TwitterData> {
-  const { id } = influencer.social_account.social_account;
-  log.info(`Fetching tweets for influencer (${id})...`);
-  const res = await fetchFromCache(
-    `https://api.twitter.com/2/users/${id}/tweets?` +
-      `tweet.fields=created_at,entities,author_id,public_metrics,referenced_tweets&` +
-      `expansions=referenced_tweets.id,referenced_tweets.id.author_id&` +
-      `max_results=5`,
-    { headers: { authorization: `Bearer ${TWITTER_TOKEN}` } }
-  );
-  const headers = Object.fromEntries(res.headers.entries());
-  log.trace(`Headers: ${JSON.stringify(headers, null, 2)}`);
-  const data = (await res.json()) as TwitterData;
-  if (data.errors && data.title && data.detail && data.type)
-    log.error(`${data.title}: ${data.detail} (${data.type})`);
+async function getArticles(i: Influencer, host: string): Promise<Article[]> {
+  const { id } = i.social_account.social_account;
+  log.debug(`Fetching articles for influencer (${id})...`);
+  const res = await fetchFromCache(`${host}/influencers/${id}/articles`);
+  const data = (await res.json()) as Article[];
   return data;
-}
-
-async function getArticle(url: string, tweets: Tweet[]): Promise<Article> {
-  log.debug(`Fetching metadata for link (${url})...`);
-  const article = {
-    url,
-    tweets,
-    title: `${decode(url.substr(0, 10))}...`,
-    description:
-      'No appropriate description meta tag found in article html; perhaps' +
-      ' they did something weird like put their tag names in all caps 🤷.',
-    // TODO: Perhaps show the most recent share or the first share or the date
-    // the article or content link was actually published (use metascraper).
-    domain: new URL(url).hostname.replace(/^www\./, ''),
-  };
-  try {
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 5000);
-    const res = await fetchFromCache(url, {
-      signal: controller.signal,
-    });
-    const headers = Object.fromEntries(res.headers.entries());
-    log.trace(`Headers (${url}): ${JSON.stringify(headers, null, 2)}`);
-    clearTimeout(timeoutId);
-    let title = '';
-    let description = '';
-    await new HTMLRewriter()
-      .on('meta', {
-        element(el) {
-          const content = el.getAttribute('content');
-          if (content && el.getAttribute('property') === 'og:description')
-            description = content;
-          if (content && el.getAttribute('name') === 'description')
-            description = content;
-          if (content && el.getAttribute('property') === 'og:title')
-            title = content;
-        },
-      })
-      .on('title', {
-        text(txt) {
-          title += txt.text;
-        },
-      })
-      .transform(res)
-      .text();
-    article.title = decode(title) || article.title;
-    article.description = decode(description) || article.description;
-  } catch (e) {
-    log.error(`Error fetching link (${url}): ${e.message}`);
-  }
-  return article;
-}
-
-function isArticleURL(l?: URL): boolean {
-  return !!l && !!l.expanded_url && !/twitter.com/.test(l.expanded_url);
-}
-
-async function getArticles(influencer: Influencer): Promise<Article[]> {
-  const tweets = await getTweets(influencer);
-  const all = [...(tweets.data ?? []), ...(tweets.includes?.tweets ?? [])];
-  const links: { url: string; tweets: Tweet[] }[] = [];
-  (tweets.data ?? [])
-    .filter((t) => t.entities?.urls?.some(isArticleURL))
-    .forEach((t: Tweet) => {
-      const url = t.entities?.urls?.filter(isArticleURL)[0].expanded_url;
-
-      if (influencer.social_account.social_account.id !== t.author_id)
-        throw new Error(
-          `Expected tweet (${t.id}) author (${t.author_id}) to be influencer ` +
-            `(${influencer.social_account.social_account.id})`
-        );
-      invariant(url, `expected tweet (${t.id}) to have url`);
-
-      const exists = links.find((l) => l.url === url);
-      const link = exists ?? { url, tweets: [t] };
-
-      logTweet(t, influencer, url, link.tweets);
-
-      function count(rt: Omit<TweetRef, 'type'> & Partial<TweetRef>) {
-        if (rt.type === 'replied_to') {
-          log.debug(`Skipping replied_to tweet (${rt.id})`);
-          return;
-        }
-        if (link.tweets.some((tt) => rt.id === tt.id)) {
-          log.debug(`Skipping already counted tweet (${rt.id})`);
-          return;
-        }
-        const tweet = all.find((tt) => rt.id === tt.id);
-        if (!tweet) {
-          log.warn(`Missing ${rt.type ?? 'original'} tweet (${rt.id})`);
-          return;
-        }
-        tweet.referenced_tweets?.forEach(count);
-        if (link.tweets.some((tt) => tt.author_id === tweet.author_id)) {
-          log.debug(`Skipping already counted author (${tweet.author_id})`);
-          return;
-        }
-        log.debug(
-          `Adding ${rt.type ?? 'original'} tweet (${rt.id}) author (https:` +
-            `//hive.one/p/${tweet.author_id}) score to link (${link.url})`
-        );
-        link.tweets.push(tweet);
-        log.debug(`${link.tweets.length} tweets for link (${link.url})`);
-      }
-
-      count(t);
-
-      if (!exists) links.push(link);
-    });
-  return Promise.all(links.map((l) => getArticle(l.url, l.tweets)));
 }
 
 export const action: ActionFunction = async ({ params, request }) => {
   invariant(params.topic, 'expected params.topic');
   const n = Number(new URL(request.url).searchParams.get('n') ?? 0);
   const articles = ((await request.json()) ?? []) as Article[];
+  const url = new URL(request.url);
+  const host = `${url.protocol}//${url.host}`;
 
   // 1. Fetch all 12989 influencers from Hive (in batches of 100).
   const { influencers, has_more: more } = await getInfluencers(params.topic, n);
 
   // 2. For each influencer, fetch all 3200 tweets from Twitter timeline (in
   // batches of 100) and each tweet's article's metadata (e.g. title).
-  (await Promise.all(influencers.map(getArticles)))
+  (await Promise.all(influencers.map((i) => getArticles(i, host))))
     .flat()
     .forEach((a: Article) => {
       const exists = articles.find((b) => b.url === a.url);
@@ -240,6 +86,7 @@ export const action: ActionFunction = async ({ params, request }) => {
         );
         if (!t.author) log.warn(`Author (${t.author_id}) not an influencer.`);
         if (exists?.tweets.every((c) => c.id !== t.id)) exists.tweets.push(t);
+        logTweet(t, a);
       });
       if (!exists) articles.push(a);
     });
@@ -250,8 +97,6 @@ export const action: ActionFunction = async ({ params, request }) => {
 
   // 4. Recursively continue to fetch influencers and their articles.
   if (!more || n >= 20) return json(articles);
-  const url = new URL(request.url);
-  const host = `${url.protocol}//${url.host}`;
   return fetch(`${host}/articles/${params.topic}?n=${n + 5}`, {
     method: 'POST',
     body: JSON.stringify(articles),
---
+++ app/routes/influencers.$id.articles.ts
@@ -0,0 +1,152 @@
+import type { LoaderFunction } from 'remix';
+import invariant from 'tiny-invariant';
+import { json } from 'remix';
+
+import type { Article, Tweet, TweetRef, URL } from '~/types.server';
+import { decode, fetchFromCache, log } from '~/utils.server';
+
+interface TwitterData {
+  data?: Tweet[];
+  includes?: {
+    users?: { id: string; name: string; username: string }[];
+    tweets?: Tweet[];
+  };
+  meta?: {
+    newest_id?: string;
+    oldest_id?: string;
+    result_count?: number;
+    next_token?: string;
+  };
+  errors?: { parameters: Record<string, string[]>; message: string }[];
+  title?: string;
+  detail?: string;
+  type?: string;
+}
+
+async function getTweets(id: string): Promise<TwitterData> {
+  log.info(`Fetching tweets for influencer (${id})...`);
+  const res = await fetchFromCache(
+    `https://api.twitter.com/2/users/${id}/tweets?` +
+      `tweet.fields=created_at,entities,author_id,public_metrics,referenced_tweets&` +
+      `expansions=referenced_tweets.id,referenced_tweets.id.author_id&` +
+      `max_results=15`,
+    { headers: { authorization: `Bearer ${TWITTER_TOKEN}` } }
+  );
+  const headers = Object.fromEntries(res.headers.entries());
+  log.trace(`Headers: ${JSON.stringify(headers, null, 2)}`);
+  const data = (await res.json()) as TwitterData;
+  if (data.errors && data.title && data.detail && data.type)
+    log.error(`${data.title}: ${data.detail} (${data.type})`);
+  return data;
+}
+
+async function getArticle(url: string, tweets: Tweet[]): Promise<Article> {
+  log.debug(`Fetching metadata for link (${url})...`);
+  const article = {
+    url,
+    tweets,
+    title: `${decode(url.substr(0, 10))}...`,
+    description:
+      'No appropriate description meta tag found in article html; perhaps' +
+      ' they did something weird like put their tag names in all caps 🤷.',
+    // TODO: Perhaps show the most recent share or the first share or the date
+    // the article or content link was actually published (use metascraper).
+    domain: new URL(url).hostname.replace(/^www\./, ''),
+  };
+  try {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 5000);
+    const res = await fetchFromCache(url, {
+      signal: controller.signal,
+    });
+    const headers = Object.fromEntries(res.headers.entries());
+    log.trace(`Headers (${url}): ${JSON.stringify(headers, null, 2)}`);
+    clearTimeout(timeoutId);
+    let title = '';
+    let description = '';
+    await new HTMLRewriter()
+      .on('meta', {
+        element(el) {
+          const content = el.getAttribute('content');
+          if (content && el.getAttribute('property') === 'og:description')
+            description = content;
+          if (content && el.getAttribute('name') === 'description')
+            description = content;
+          if (content && el.getAttribute('property') === 'og:title')
+            title = content;
+        },
+      })
+      .on('title', {
+        text(txt) {
+          title += txt.text;
+        },
+      })
+      .transform(res)
+      .text();
+    article.title = decode(title) || article.title;
+    article.description = decode(description) || article.description;
+  } catch (e) {
+    log.error(`Error fetching link (${url}): ${e.message}`);
+  }
+  return article;
+}
+
+function isArticleURL(l?: URL): boolean {
+  return !!l && !!l.expanded_url && !/twitter.com/.test(l.expanded_url);
+}
+
+async function getArticles(id: string): Promise<Article[]> {
+  const tweets = await getTweets(id);
+  const all = [...(tweets.data ?? []), ...(tweets.includes?.tweets ?? [])];
+  const links: { url: string; tweets: Tweet[] }[] = [];
+  (tweets.data ?? [])
+    .filter((t) => t.entities?.urls?.some(isArticleURL))
+    .forEach((t: Tweet) => {
+      if (id !== t.author_id)
+        throw new Error(`Tweet (${t.id}) author (${t.author_id}) not (${id})`);
+
+      const url = t.entities?.urls?.filter(isArticleURL)[0].expanded_url;
+      invariant(url, `expected tweet (${t.id}) to have url`);
+      const exists = links.find((l) => l.url === url);
+      const link = exists ?? { url, tweets: [t] };
+
+      function count(rt: Omit<TweetRef, 'type'> & Partial<TweetRef>) {
+        if (rt.type === 'replied_to') {
+          log.debug(`Skipping replied_to tweet (${rt.id})`);
+          return;
+        }
+        if (link.tweets.some((tt) => rt.id === tt.id)) {
+          log.debug(`Skipping already counted tweet (${rt.id})`);
+          return;
+        }
+        const tweet = all.find((tt) => rt.id === tt.id);
+        if (!tweet) {
+          log.warn(`Missing ${rt.type ?? 'original'} tweet (${rt.id})`);
+          return;
+        }
+        tweet.referenced_tweets?.forEach(count);
+        if (link.tweets.some((tt) => tt.author_id === tweet.author_id)) {
+          log.debug(`Skipping already counted author (${tweet.author_id})`);
+          return;
+        }
+        log.debug(
+          `Adding ${rt.type ?? 'original'} tweet (${rt.id}) author (https:` +
+            `//hive.one/p/${tweet.author_id}) score to link (${link.url})`
+        );
+        link.tweets.push(tweet);
+        log.debug(`${link.tweets.length} tweets for link (${link.url})`);
+      }
+
+      count(t);
+
+      if (!exists) links.push(link);
+    });
+  return Promise.all(links.map((l) => getArticle(l.url, l.tweets)));
+}
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.id, 'expected params.id');
+  log.info(`Fetching articles for influencer (${params.id})...`);
+  const articles = await getArticles(params.id);
+  return json(articles);
+};
