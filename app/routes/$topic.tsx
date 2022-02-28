45285b7c0e7b08814b60200b9bfe2ce4930534c5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-02-28 12:10:02 -0800

    fix(routes/$topic): account for `undefined` author

--- app/routes/$topic.tsx
+++ app/routes/$topic.tsx
@@ -49,23 +49,26 @@ export default function Index() {
             <p className='text-sm ml-2'>{article.description}</p>
             <div className='text-sm text-stone-600 lowercase flex items-center mt-1.5 ml-2'>
               <span className='flex flex-row-reverse justify-end -ml-[2px] mr-0.5'>
-                {article.tweets.map((tweet) => (
-                  <a
-                    className='inline-block cursor-pointer duration-75 hover:transition hover:border-0 hover:scale-125 hover:z-0 h-6 w-6 rounded-full bg-white border-2 border-stone-200 -mr-2 first:mr-0 overflow-hidden'
-                    href={`https://twitter.com/${tweet.author.social_account.social_account.screen_name}/status/${tweet.id}`}
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    key={tweet.id}
-                  >
-                    <img
-                      src={`/img/${encodeURIComponent(
-                        tweet.author.social_account.social_account
-                          .profile_image_url
-                      )}`}
-                      alt=''
-                    />
-                  </a>
-                ))}
+                {article.tweets.map(
+                  (tweet) =>
+                    tweet.author && (
+                      <a
+                        className='inline-block cursor-pointer duration-75 hover:transition hover:border-0 hover:scale-125 hover:z-0 h-6 w-6 rounded-full bg-white border-2 border-stone-200 -mr-2 first:mr-0 overflow-hidden'
+                        href={`https://twitter.com/${tweet.author.social_account.social_account.screen_name}/status/${tweet.id}`}
+                        rel='noopener noreferrer'
+                        target='_blank'
+                        key={tweet.id}
+                      >
+                        <img
+                          src={`/img/${encodeURIComponent(
+                            tweet.author.social_account.social_account
+                              .profile_image_url
+                          )}`}
+                          alt=''
+                        />
+                      </a>
+                    )
+                )}
               </span>
               <a
                 className='ml-1 hover:underline cursor-pointer'
