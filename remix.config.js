3c57e092ffd4bf1367c61c5ba0938680cc67bddf
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-07 09:45:19 -0800

    deps: use fly.io for deployments

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,6 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: 'cloudflare-workers',
-  server: './server.js',
-  devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ['.*'],
   // appDirectory: 'app',
   // assetsBuildDirectory: 'public/build',
