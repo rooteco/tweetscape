5f44906bf49566dd6bc852dbfb6bc53738d88351
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-07 17:43:47 -0800

    refactor(app/routes): rename `$topic` to `$cluster`

--- app/routes/$cluster.tsx
+++ app/routes/$cluster.tsx
@@ -2,13 +2,18 @@ import type { LoaderFunction } from 'remix';
 import invariant from 'tiny-invariant';
 import { useLoaderData } from 'remix';
 
-import type { Article } from '~/types.server';
+import { decode, log } from '~/utils.server';
+import type { Article } from '~/db.server';
+import { pool } from '~/db.server';
 
-export const loader: LoaderFunction = async ({ params, request }) => {
+export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.topic, 'expected params.topic');
-  const url = new URL(request.url);
-  const api = `${url.protocol}//${url.host}/articles/${params.topic}?page=0`;
-  return fetch(api, { method: 'POST', body: JSON.stringify([]) });
+  const db = await pool.connect();
+  const data = await db.query(
+    `select * from articles where cluster_slug = '${params.topic}'`
+  );
+  log.debug(`Data: ${JSON.stringify(data, null, 2)}`);
+  return data.rows as Article[];
 };
 
 export default function Index() {
@@ -22,30 +27,40 @@ export default function Index() {
           </div>
         )}
         {articles.map((article) => (
-          <li key={article.url} className='my-4'>
+          <li key={article.id} className='my-4'>
             <div className='ml-2'>
               <a
                 className='font-serif font-semibold hover:underline text-base'
-                href={article.url}
+                href={article.expanded_url}
                 target='_blank'
                 rel='noopener noreferrer'
               >
-                {article.title}
+                {article.title ||
+                  `${decode(
+                    article.expanded_url
+                      .replace(/^https?:\/\/(www\.)?/, '')
+                      .substr(0, 50)
+                  )}…`}
               </a>{' '}
               <span className='text-sm'>
                 (
                 <a
                   className='hover:underline'
-                  href={`https://${article.domain}`}
+                  href={`https://${new URL(
+                    article.expanded_url
+                  ).hostname.replace(/^www\./, '')}`}
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  {article.domain}
+                  {new URL(article.expanded_url).hostname.replace(/^www\./, '')}
                 </a>
                 )
               </span>
             </div>
-            <p className='text-sm ml-2'>{article.description}</p>
+            <p className='text-sm ml-2'>
+              {article.description ||
+                'No appropriate description meta tag found in article html; perhaps they did something weird like put their tag names in all caps 🤷.'}
+            </p>
             <div className='text-sm text-stone-600 lowercase flex items-center mt-1.5 ml-2'>
               <span className='flex flex-row-reverse justify-end -ml-[2px] mr-0.5'>
                 {article.tweets.map(
@@ -53,15 +68,14 @@ export default function Index() {
                     tweet.author && (
                       <a
                         className='inline-block cursor-pointer duration-75 hover:transition hover:border-0 hover:scale-125 hover:z-0 h-6 w-6 rounded-full bg-white border-2 border-stone-200 -mr-2 first:mr-0 overflow-hidden'
-                        href={`https://twitter.com/${tweet.author.social_account.social_account.screen_name}/status/${tweet.id}`}
+                        href={`https://twitter.com/${tweet.author.username}/status/${tweet.id}`}
                         rel='noopener noreferrer'
                         target='_blank'
                         key={tweet.id}
                       >
                         <img
                           src={`/img/${encodeURIComponent(
-                            tweet.author.social_account.social_account
-                              .profile_image_url
+                            tweet.author.profile_image_url
                           )}`}
                           alt=''
                         />
@@ -72,7 +86,7 @@ export default function Index() {
               <a
                 className='ml-1 hover:underline cursor-pointer'
                 href={`https://twitter.com/search?q=${encodeURIComponent(
-                  article.url
+                  article.expanded_url
                 )}`}
                 rel='noopener noreferrer'
                 target='_blank'
