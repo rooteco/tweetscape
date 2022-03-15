76b348977aa6f9adbb489221f3277ee89ed7f109
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-14 17:35:01 -0700

    refactor(server.js): remove unused express server

--- server.js
+++
@@ -1,6 +0,0 @@
-import { createEventHandler } from '@remix-run/cloudflare-workers';
-import * as build from '@remix-run/dev/server-build';
-
-const handler = createEventHandler({ build, mode: process.env.NODE_ENV });
-
-addEventListener('fetch', handler);
