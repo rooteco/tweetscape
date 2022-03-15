121afa4dbc186669656f00c2fa1503795759675b
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-15 14:39:15 -0700

    wip(tests/index): add placeholder for cookie tests
    Blocked by cypress-io/cypress#18690 and cypress-io/cypress#19316

--- cypress/tests/index.spec.ts
+++ cypress/tests/index.spec.ts
@@ -1,6 +1,13 @@
 describe('Index PG', () => {
-  it('redirects to most recent cluster', () => {
+  it('redirects to tesla by default', () => {
     cy.visit('/');
     cy.url().should('eq', 'http://localhost:3000/tesla');
+  });
+
+  it('redirects to most recent view', () => {
+    // TODO: Test that visiting a cluster view with specific filters sets the
+    // `href` cookie properly which is then used by the index page to redirect.
+    // @see {@link https://github.com/cypress-io/cypress/issues/19316}
+    // @see {@link https://github.com/cypress-io/cypress/issues/18690}
   });
 });
