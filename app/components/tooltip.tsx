5a44b2e8ea7afa700efa35284a6d2472bf78e161
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-15 21:29:24 -0700

    feat: add filter description tooltips (#32)
    * wip(components/nav): add `title` tooltips
    
    * wip(components/article): add `title` tooltips
    
    * wip(nav): add custom tooltip to "attention score"
    
    * feat(app): use custom `Tooltip` component
    
    * feat(article): replace `title=` with `Tooltip` component
    
    * fix(tooltip): specify `z-10` to always be on top
    
    * feat(article): add attention scores explanation
    
    * feat(article): link directly to top-level "show retweets"
    
    * fix(nav): use property parenthesis

---
+++ app/components/tooltip.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+
+export interface TooltipProps {
+  content: ReactNode;
+  children: ReactNode;
+}
+
+export default function Tooltip({ content, children }: TooltipProps) {
+  return (
+    <span className='relative group'>
+      {children}
+      <div className='absolute z-10 transition-all group-hover:delay-300 group-hover:visible group-hover:opacity-100 group-hover:translate-y-0 -translate-y-1 shadow-2xl opacity-0 delay-75 duration-100 invisible top-6 dark:bg-slate-900 bg-white rounded border border-slate-200 dark:border-white p-2.5 w-max max-w-xs'>
+        {content}
+      </div>
+    </span>
+  );
+}
