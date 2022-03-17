ab5952e6e7b294916b11114b6cdf3b1dabc60270
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-17 16:14:14 -0700

    feat: use `prisma` instead of `pg` (#35)
    * deps: add `prisma` as dev package
    
    * wip(prisma/schema): initial commit of template
    
    * chore(prisma/schema): schema from db introspection
    
    * chore(package.json): add prisma to `postinstall` script
    
    * refactor(app/db): export `db` function directly
    
    * refactor(app): combine `redis.server` and `db.server`
    
    * refactor: rename `db` to `redis`
    
    * deps: add `@prisma/client` sdk package
    
    * feat(app/db): export prisma client singleton
    
    * feat(db): create separate link `images` table
    
    * fix(prisma/schema): rerun `db pull` for new table
    
    * feat(app): `redis` wraps `prisma.$queryRawUnsafe`
    
    Fixes TS-78
    
    * deps: specify `pg` and `pg-format` as dev

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,109 +1,23 @@
-import { createHash } from 'crypto';
+import { PrismaClient } from '@prisma/client';
 
-import { Client } from 'pg';
-import type { QueryResult } from 'pg';
-import type { createClient } from 'redis';
+let db: PrismaClient;
 
-import { log } from '~/utils.server';
-import redisClient from '~/redis.server';
-import { substr } from '~/utils';
-
-interface RequestResponseCache<T> {
-  (query: string, maxAgeSeconds?: number): Promise<QueryResult<T>>;
+declare global {
+  var __db: PrismaClient | undefined;
 }
 
-function createSwrRedisCache<T>({
-  redisClient,
-}: {
-  redisClient: ReturnType<typeof createClient>;
-}): RequestResponseCache<T> {
-  let connectionPromise: Promise<void>;
-  if (!redisClient.isOpen) {
-    connectionPromise = redisClient.connect();
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+if (process.env.NODE_ENV === 'production') {
+  db = new PrismaClient();
+  db.$connect();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+    global.__db.$connect();
   }
-
-  return async (query, maxAgeSeconds = 60) => {
-    await connectionPromise;
-
-    const key = createHash('sha256').update(query).digest('hex');
-
-    const stillGoodKey = `swr:stillgood:${key}`;
-    const responseKey = `swr:response:${key}`;
-
-    const cachedStillGoodPromise = redisClient
-      .get(stillGoodKey)
-      .then((cachedStillGood) => {
-        if (!cachedStillGood) {
-          return false;
-        }
-        return true;
-      })
-      .catch(() => false);
-
-    let response = await redisClient
-      .get(responseKey)
-      .then(async (cachedResponseString) => {
-        if (!cachedResponseString) {
-          return null;
-        }
-
-        const cachedResponse = JSON.parse(
-          cachedResponseString
-        ) as QueryResult<T>;
-
-        if (await cachedStillGoodPromise) {
-          log.debug(`Redis cache hit for key (${responseKey}), returning...`);
-        } else {
-          log.debug(
-            `Redis cache stale for key (${responseKey}), revalidating...`
-          );
-
-          /* eslint-disable-next-line promise/no-nesting */
-          (async () => {
-            log.debug(`Establishing connection with PostgreSQL...`);
-            const client = new Client({
-              connectionString: process.env.DATABASE_URL,
-            });
-            client.on('error', (e) =>
-              log.error(`PostgreSQL error: ${e.stack}`)
-            );
-            await client.connect();
-            log.debug(`Executing PostgreSQL query (${substr(query, 50)})...`);
-            const toCache = await client.query(query);
-            log.debug(`Disconnecting client from PostgreSQL...`);
-            await client.end();
-            await redisClient.set(responseKey, JSON.stringify(toCache));
-            await redisClient.setEx(stillGoodKey, maxAgeSeconds, 'true');
-          })().catch((e) => {
-            log.error(`Failed to revalidate: ${(e as Error).stack}`);
-          });
-        }
-
-        return cachedResponse;
-      })
-      .catch(() => null);
-
-    if (!response) {
-      log.debug(`Establishing connection with PostgreSQL...`);
-      const client = new Client({ connectionString: process.env.DATABASE_URL });
-      client.on('error', (e) => log.error(`PostgreSQL error: ${e.stack}`));
-      await client.connect();
-      log.debug(`Executing PostgreSQL query (${substr(query, 50)})...`);
-      response = await client.query(query);
-      log.debug(`Disconnecting client from PostgreSQL...`);
-      await client.end();
-      log.debug(`Redis cache miss for key (${responseKey}), querying...`);
-
-      (async () => {
-        await redisClient.set(responseKey, JSON.stringify(response));
-        await redisClient.setEx(stillGoodKey, maxAgeSeconds, 'true');
-      })().catch((e) => {
-        log.error(`Failed to seed cache: ${(e as Error).stack}`);
-      });
-    }
-
-    return response;
-  };
+  db = global.__db;
 }
 
-export const db = createSwrRedisCache({ redisClient });
+export { db };
