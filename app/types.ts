f506204ae0b5a19f4fd74f94407b90163f843264
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-17 20:02:34 -0700

    feat(app/types): simply re-export prisma types

--- app/types.ts
+++ app/types.ts
@@ -1,81 +1,43 @@
-// It's safer to make these all strings so that I always wrap them in the
-// front-end code that receives the JSON-ified versions of all these datatypes.
-export type Date = string;
+import type {
+  annotation_type,
+  annotations,
+  clusters,
+  images,
+  influencers,
+  links,
+  lists,
+  mentions,
+  ref_type,
+  refs,
+  scores,
+  tag_type,
+  tags,
+  tokens,
+  tweets,
+  urls,
+} from '@prisma/client';
 
-export interface Cluster {
-  id: string;
-  name: string;
-  slug: string;
-  active: boolean;
-  created_at: Date;
-  updated_at: Date;
-}
-
-export interface Link {
-  id: number;
-  url: string;
-  expanded_url: string;
-  display_url: string;
-  images: { url: string; width: number; height: number }[] | null;
-  status: number | null;
-  title: string | null;
-  description: string | null;
-  unwound_url: string | null;
-}
-
-export interface Tweet {
-  id: string;
-  author_id: string;
-  text: string;
-  html?: string;
-  retweet_count: number;
-  reply_count: number;
-  like_count: number;
-  quote_count: number;
-  created_at: Date;
-}
-
-export interface Score {
-  id: string;
-  influencer_id: string;
-  cluster_id: string;
-  attention_score: number;
-  attention_score_change_week: number;
-  insider_score: number;
-  organization_rank: number | null;
-  personal_rank: number | null;
-  rank: number;
-  created_at: Date;
-}
-
-export interface Token {
-  id: number;
-  token_type: string;
-  expires_in: number;
-  access_token: string;
-  scope: string;
-  refresh_token: string;
-  created_at: Date;
-  updated_at: Date;
-}
-
-export interface Influencer {
-  id: string;
-  name: string;
-  username: string;
-  profile_image_url: string | null;
-  followers_count: number | null;
-  following_count: number | null;
-  tweets_count: number | null;
-  created_at: Date | null;
-  updated_at: Date | null;
-}
-
-export interface Article extends Link {
+export type Annotation = annotations;
+export type Cluster = clusters;
+export type Influencer = influencers;
+export type Link = links;
+export type Mention = mentions;
+export type Ref = refs;
+export type Score = scores;
+export type Tag = tags;
+export type Tweet = tweets;
+export type URL = urls;
+export type Image = images;
+export type Token = tokens;
+export type List = lists;
+export type AnnotationType = annotation_type;
+export type RefType = ref_type;
+export type TagType = tag_type;
+export type Article = Link & {
   cluster_id: string;
   cluster_name: string;
   cluster_slug: string;
   insider_score: number;
   attention_score: number;
   tweets: (Tweet & { author: Influencer; score: Score })[];
-}
+};
