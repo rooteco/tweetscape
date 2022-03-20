8dc5f1165e6ed8f8c93641d024f2501c3d048496
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-20 10:32:24 -0700

    fix(app): cancel in-flight requests after logout
    Fixes TS-116

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useFetcher, useMatches } from 'remix';
+import { Link, NavLink, useFetcher, useMatches } from 'remix';
 import cn from 'classnames';
 
 import { Theme, useTheme } from '~/theme';
@@ -124,7 +124,39 @@ export default function Header() {
               <span>Logout</span>
             </button>
           )}
-          <Sync />
+          {!root?.user && (
+            <Link
+              className='ml-1.5 inline-flex truncate items-center text-white text-xs bg-[#1d9bf0] rounded px-2 h-6'
+              to='/oauth'
+            >
+              <svg
+                className='shrink-0 w-3.5 h-3.5 mr-1 fill-white'
+                viewBox='328 355 335 276'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='
+              M 630, 425
+              A 195, 195 0 0 1 331, 600
+              A 142, 142 0 0 0 428, 570
+              A  70,  70 0 0 1 370, 523
+              A  70,  70 0 0 0 401, 521
+              A  70,  70 0 0 1 344, 455
+              A  70,  70 0 0 0 372, 460
+              A  70,  70 0 0 1 354, 370
+              A 195, 195 0 0 0 495, 442
+              A  67,  67 0 0 1 611, 380
+              A 117, 117 0 0 0 654, 363
+              A  65,  65 0 0 1 623, 401
+              A 117, 117 0 0 0 662, 390
+              A  65,  65 0 0 1 630, 425
+              Z'
+                />
+              </svg>
+              <span>Login with Twitter</span>
+            </Link>
+          )}
+          {root?.user && <Sync />}
         </nav>
       </div>
       <div className='flex min-w-0 ml-4'>
--- app/components/sync.tsx
+++ app/components/sync.tsx
@@ -1,6 +1,6 @@
 import * as timeago from 'timeago.js';
-import { Link, useFetcher, useLocation, useMatches } from 'remix';
 import { useContext, useEffect, useState } from 'react';
+import { useFetcher, useLocation, useMatches } from 'remix';
 import TimeAgo from 'timeago-react';
 import en_short from 'timeago.js/lib/lang/en_short';
 
@@ -51,39 +51,6 @@ export default function Sync() {
 
   const location = useLocation();
 
-  if (!user)
-    return (
-      <Link
-        className='ml-1.5 inline-flex truncate items-center text-white text-xs bg-[#1d9bf0] rounded px-2 h-6'
-        to='/oauth'
-      >
-        <svg
-          className='shrink-0 w-3.5 h-3.5 mr-1 fill-white'
-          viewBox='328 355 335 276'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path
-            d='
-        M 630, 425
-        A 195, 195 0 0 1 331, 600
-        A 142, 142 0 0 0 428, 570
-        A  70,  70 0 0 1 370, 523
-        A  70,  70 0 0 0 401, 521
-        A  70,  70 0 0 1 344, 455
-        A  70,  70 0 0 0 372, 460
-        A  70,  70 0 0 1 354, 370
-        A 195, 195 0 0 0 495, 442
-        A  67,  67 0 0 1 611, 380
-        A 117, 117 0 0 0 654, 363
-        A  65,  65 0 0 1 623, 401
-        A 117, 117 0 0 0 662, 390
-        A  65,  65 0 0 1 630, 425
-        Z'
-          />
-        </svg>
-        <span>Login with Twitter</span>
-      </Link>
-    );
   if (error)
     return (
       <a
