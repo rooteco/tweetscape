435b4506e994e704c201f60a6cddb65178fa846b
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-21 12:23:37 -0700

    refactor(tweet): reuse custom `<Action>` component

--- app/components/tweet.tsx
+++ app/components/tweet.tsx
@@ -1,9 +1,37 @@
+import type { ReactNode } from 'react';
+
 import type { Influencer, Tweet } from '~/types';
 import LikeIcon from '~/icons/like';
 import ReplyIcon from '~/icons/reply';
 import RetweetIcon from '~/icons/retweet';
 import ShareIcon from '~/icons/share';
 import { TimeAgo } from '~/components/timeago';
+
+type ActionProps = {
+  count?: number;
+  color: string;
+  icon: ReactNode;
+  href: string;
+};
+
+function Action({ count, color, icon, href }: ActionProps) {
+  return (
+    <a
+      data-cy='share'
+      className={`mr-5 grow shrink basis-0 inline-flex justify-start items-center transition duration-[0.2s] group hover:${color}-550`}
+      href={href}
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <div
+        className={`p-1.5 mr-0.5 rounded-full transition duration-[0.2s] group-hover:bg-${color}-50`}
+      >
+        {icon}
+      </div>
+      {count}
+    </a>
+  );
+}
 
 export type TweetItemProps = Tweet & {
   author: Influencer;
@@ -70,52 +98,28 @@ export default function TweetItem({
           dangerouslySetInnerHTML={{ __html: html ?? text }}
         />
         <div className='-m-1.5 flex items-stretch min-w-0 justify-between text-slate-500'>
-          <a
-            data-cy='reply'
-            className='mr-5 grow shrink basis-0 inline-flex justify-start items-center transition duration-[0.2s] group hover:text-blue-550'
+          <Action
+            color='blue'
+            icon={<ReplyIcon />}
             href={`https://twitter.com/intent/tweet?in_reply_to=${id}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <div className='p-1.5 mr-0.5 rounded-full transition duration-[0.2s] group-hover:bg-blue-50'>
-              <ReplyIcon />
-            </div>
-          </a>
-          <a
-            data-cy='twitter'
-            className='mr-5 grow shrink basis-0 inline-flex justify-start items-center transition duration-[0.2s] group hover:text-green-550'
+          />
+          <Action
+            color='green'
+            icon={<RetweetIcon />}
             href={`https://twitter.com/intent/retweet?tweet_id=${id}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <div className='p-1.5 mr-0.5 rounded-full transition duration-[0.2s] group-hover:bg-green-50'>
-              <RetweetIcon />
-            </div>
-            {retweet_count + quote_count}
-          </a>
-          <a
-            data-cy='like'
-            className='mr-5 grow shrink basis-0 inline-flex justify-start items-center transition duration-[0.2s] group hover:text-red-550'
+            count={retweet_count + quote_count}
+          />
+          <Action
+            color='red'
+            icon={<LikeIcon />}
             href={`https://twitter.com/intent/like?tweet_id=${id}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <div className='p-1.5 mr-0.5 rounded-full transition duration-[0.2s] group-hover:bg-red-50'>
-              <LikeIcon />
-            </div>
-            {like_count}
-          </a>
-          <a
-            data-cy='share'
-            className='mr-5 grow shrink basis-0 inline-flex justify-start items-center transition duration-[0.2s] group hover:text-blue-550'
+            count={like_count}
+          />
+          <Action
+            color='blue'
+            icon={<ShareIcon />}
             href={`https://twitter.com/${author.username}/status/${id}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <div className='p-1.5 mr-0.5 rounded-full transition duration-[0.2s] group-hover:bg-blue-50'>
-              <ShareIcon />
-            </div>
-          </a>
+          />
         </div>
       </article>
     </li>
