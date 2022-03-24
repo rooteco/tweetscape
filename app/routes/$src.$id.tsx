f09428bbc7ddcd08318a6b2c4635526ed51000d8
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-23 18:47:51 -0700

    fix($src.$id): hide native scroller overflow

--- app/routes/$src.$id.tsx
+++ app/routes/$src.$id.tsx
@@ -272,7 +272,7 @@ export default function Cluster() {
                 ))}
               scrollThreshold={0.65}
               scrollableTarget='tweets'
-              style={{ overflow: undefined }}
+              style={{ overflow: 'hidden' }}
               hasMore
             >
               {tweets.map((t) => (
