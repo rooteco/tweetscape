74eee7323183e75c2ae5949558d40d1147b6cbbe
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2022-03-30 23:22:33 -0700

    wip(column): add fade in opacity transition

--- app/components/column.tsx
+++ app/components/column.tsx
@@ -1,5 +1,6 @@
+import { Outlet, useResolvedPath } from 'remix';
 import type { ReactNode, Ref } from 'react';
-import { Outlet } from 'remix';
+import { animated, useTransition } from '@react-spring/web';
 import cn from 'classnames';
 import { forwardRef } from 'react';
 
@@ -13,18 +14,28 @@ function Column<T>(
   { id, context, className, children }: ColumnProps<T>,
   ref?: Ref<HTMLElement>
 ) {
+  const transitions = useTransition(<>{children}</>, {
+    key: useResolvedPath('').pathname,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
   return (
     <>
-      <section
-        ref={ref}
-        id={id}
-        className={cn(
-          'first-of-type:ml-auto last-of-type:mr-auto flex-none flex flex-col overflow-y-scroll',
-          className
-        )}
-      >
-        {children}
-      </section>
+      {transitions((styles, item, t, key) => (
+        <animated.section
+          style={styles}
+          ref={ref}
+          key={key}
+          id={id}
+          className={cn(
+            'first-of-type:ml-auto last-of-type:mr-auto flex-none flex flex-col overflow-y-scroll',
+            className
+          )}
+        >
+          {item}
+        </animated.section>
+      ))}
       <Outlet context={context} />
     </>
   );
